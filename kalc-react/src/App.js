113b843f9704c285df7ec0470b62bc752a959847
Ajeesh Kunnathodi <kajeesh@uber.com>
2020-08-26 18:03:11 +0530

    Initial COmmit

---
+++ kalc-react/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Layout from "./components";
+
+export default function Calendar() {
+  return (
+      <Layout />
+  );
+}
---
+++ kalc-react/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {BaseProvider, LightTheme} from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import App from './App'
+
+const engine = new Styletron();
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+            <App />
+        </BaseProvider>
+    </StyletronProvider>,
+    rootElement
+);
+
